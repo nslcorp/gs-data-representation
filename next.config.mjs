1fa8446d6fdfa351ebdf6f18561d025a178d84ed
serhiinikitin <botodnepr@gmail.com>
2024-01-30 13:56:45 +0100

    config setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/gs-data-representation",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
