a788bfcb94f182327eec268842a9ce277d6b580f
serhiinikitin <botodnepr@gmail.com>
2024-01-30 17:47:33 +0100

    GitHubPages = set up 3

---
+++ out/_next/static/chunks/app/layout-bcfee65b70677310.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5249:function(e,n,i){Promise.resolve().then(i.bind(i,9570)),Promise.resolve().then(i.t.bind(i,5250,23)),Promise.resolve().then(i.t.bind(i,3385,23)),Promise.resolve().then(i.t.bind(i,9646,23))},3385:function(){}},function(e){e.O(0,[985,792,971,69,744],function(){return e(e.s=5249)}),_N_E=e.O()}]);
